06b444023e4392fbd9f7b51b07b77910e9d7252a
lowysole <lowy.sole@gmail.com>
2023-03-15 23:02:05 +0100

    Update Imgui theme

--- source/modules/ModuleEditor.cpp
+++ source/modules/ModuleEditor.cpp
@@ -60,8 +60,6 @@ bool ModuleEditor::Start() {
 	ImGui_ImplSDL2_InitForOpenGL(App->window->window, App->render->context);
 	ImGui_ImplOpenGL3_Init(GLSL_VERSION);
 
-	ImGui::StyleColorsDark();
-
 	ImGuiViewport* viewport = ImGui::GetMainViewport();
 	workPos = viewport->WorkPos;
 	workSize = viewport->WorkSize;
@@ -295,12 +293,12 @@ void ModuleEditor::SetImGuiTheme() {
 
 	// Colors
 	ImVec4* colors = ImGui::GetStyle().Colors;
-	colors[ImGuiCol_Text] = ImVec4(1.00f, 1.00f, 1.00f, 1.00f);
+	colors[ImGuiCol_Text] = ImVec4(0.90f, 0.90f, 0.90f, 1.00f);
 	colors[ImGuiCol_TextDisabled] = ImVec4(0.50f, 0.50f, 0.50f, 1.00f);
-	colors[ImGuiCol_WindowBg] = ImVec4(0.10f, 0.10f, 0.10f, 1.00f);
+	colors[ImGuiCol_WindowBg] = ImVec4(0.13f, 0.13f, 0.13f, 1.00f);
 	colors[ImGuiCol_ChildBg] = ImVec4(0.00f, 0.00f, 0.00f, 0.00f);
-	colors[ImGuiCol_PopupBg] = ImVec4(0.19f, 0.19f, 0.19f, 0.92f);
-	colors[ImGuiCol_Border] = ImVec4(0.19f, 0.19f, 0.19f, 0.29f);
+	colors[ImGuiCol_PopupBg] = ImVec4(0.33f, 0.33f, 0.33f, 0.92f);
+	colors[ImGuiCol_Border] = ImVec4(0.43f, 0.43f, 0.43f, 0.29f);
 	colors[ImGuiCol_BorderShadow] = ImVec4(0.00f, 0.00f, 0.00f, 0.24f);
 	colors[ImGuiCol_FrameBg] = ImVec4(0.05f, 0.05f, 0.05f, 0.54f);
 	colors[ImGuiCol_FrameBgHovered] = ImVec4(0.19f, 0.19f, 0.19f, 0.54f);
@@ -313,11 +311,11 @@ void ModuleEditor::SetImGuiTheme() {
 	colors[ImGuiCol_ScrollbarGrab] = ImVec4(0.34f, 0.34f, 0.34f, 0.54f);
 	colors[ImGuiCol_ScrollbarGrabHovered] = ImVec4(0.40f, 0.40f, 0.40f, 0.54f);
 	colors[ImGuiCol_ScrollbarGrabActive] = ImVec4(0.56f, 0.56f, 0.56f, 0.54f);
-	colors[ImGuiCol_CheckMark] = ImVec4(0.33f, 0.67f, 0.86f, 1.00f);
+	colors[ImGuiCol_CheckMark] = ImVec4(0.33f, 0.67f, 0.86f, 0.72f);
 	colors[ImGuiCol_SliderGrab] = ImVec4(0.34f, 0.34f, 0.34f, 0.54f);
 	colors[ImGuiCol_SliderGrabActive] = ImVec4(0.56f, 0.56f, 0.56f, 0.54f);
 	colors[ImGuiCol_Button] = ImVec4(0.05f, 0.05f, 0.05f, 0.54f);
-	colors[ImGuiCol_ButtonHovered] = ImVec4(0.19f, 0.19f, 0.19f, 0.54f);
+	colors[ImGuiCol_ButtonHovered] = ImVec4(0.23f, 0.23f, 0.23f, 0.54f);
 	colors[ImGuiCol_ButtonActive] = ImVec4(0.20f, 0.22f, 0.23f, 1.00f);
 	colors[ImGuiCol_Header] = ImVec4(0.00f, 0.00f, 0.00f, 0.52f);
 	colors[ImGuiCol_HeaderHovered] = ImVec4(0.00f, 0.00f, 0.00f, 0.36f);
@@ -328,12 +326,12 @@ void ModuleEditor::SetImGuiTheme() {
 	colors[ImGuiCol_ResizeGrip] = ImVec4(0.28f, 0.28f, 0.28f, 0.29f);
 	colors[ImGuiCol_ResizeGripHovered] = ImVec4(0.44f, 0.44f, 0.44f, 0.29f);
 	colors[ImGuiCol_ResizeGripActive] = ImVec4(0.40f, 0.44f, 0.47f, 1.00f);
-	colors[ImGuiCol_Tab] = ImVec4(0.00f, 0.00f, 0.00f, 0.52f);
-	colors[ImGuiCol_TabHovered] = ImVec4(0.14f, 0.14f, 0.14f, 1.00f);
-	colors[ImGuiCol_TabActive] = ImVec4(0.20f, 0.20f, 0.20f, 0.36f);
+	colors[ImGuiCol_Tab] = ImVec4(0.46f, 0.46f, 0.46f, 0.52f);
+	colors[ImGuiCol_TabHovered] = ImVec4(0.25f, 0.25f, 0.25f, 1.00f);
+	colors[ImGuiCol_TabActive] = ImVec4(0.71f, 0.71f, 0.71f, 0.52f);
 	colors[ImGuiCol_TabUnfocused] = ImVec4(0.00f, 0.00f, 0.00f, 0.52f);
 	colors[ImGuiCol_TabUnfocusedActive] = ImVec4(0.14f, 0.14f, 0.14f, 1.00f);
-	colors[ImGuiCol_DockingPreview] = ImVec4(0.33f, 0.67f, 0.86f, 1.00f);
+	colors[ImGuiCol_DockingPreview] = ImVec4(0.45f, 0.45f, 0.45f, 1.00f);
 	colors[ImGuiCol_DockingEmptyBg] = ImVec4(1.00f, 0.00f, 0.00f, 1.00f);
 	colors[ImGuiCol_PlotLines] = ImVec4(1.00f, 0.00f, 0.00f, 1.00f);
 	colors[ImGuiCol_PlotLinesHovered] = ImVec4(1.00f, 0.00f, 0.00f, 1.00f);
@@ -346,16 +344,16 @@ void ModuleEditor::SetImGuiTheme() {
 	colors[ImGuiCol_TableRowBgAlt] = ImVec4(1.00f, 1.00f, 1.00f, 0.06f);
 	colors[ImGuiCol_TextSelectedBg] = ImVec4(0.20f, 0.22f, 0.23f, 1.00f);
 	colors[ImGuiCol_DragDropTarget] = ImVec4(0.33f, 0.67f, 0.86f, 1.00f);
-	colors[ImGuiCol_NavHighlight] = ImVec4(1.00f, 0.00f, 0.00f, 1.00f);
-	colors[ImGuiCol_NavWindowingHighlight] = ImVec4(1.00f, 0.00f, 0.00f, 0.70f);
-	colors[ImGuiCol_NavWindowingDimBg] = ImVec4(1.00f, 0.00f, 0.00f, 0.20f);
-	colors[ImGuiCol_ModalWindowDimBg] = ImVec4(1.00f, 0.00f, 0.00f, 0.35f);
+	colors[ImGuiCol_NavHighlight] = ImVec4(0.23f, 0.23f, 0.23f, 0.53f);
+	colors[ImGuiCol_NavWindowingHighlight] = ImVec4(0.23f, 0.23f, 0.23f, 0.53f);
+	colors[ImGuiCol_NavWindowingDimBg] = ImVec4(0.23f, 0.23f, 0.23f, 0.53f);
+	colors[ImGuiCol_ModalWindowDimBg] = ImVec4(0.23f, 0.23f, 0.23f, 0.53f);
 
 	ImGuiStyle& style = ImGui::GetStyle();
-	style.WindowPadding = ImVec2(8.00f, 8.00f);
-	style.FramePadding = ImVec2(5.00f, 2.00f);
+	style.WindowPadding = ImVec2(11.00f, 6.00f);
+	style.FramePadding = ImVec2(8.00f, 3.00f);
 	style.CellPadding = ImVec2(6.00f, 6.00f);
-	style.ItemSpacing = ImVec2(6.00f, 6.00f);
+	style.ItemSpacing = ImVec2(10.00f, 6.00f);
 	style.ItemInnerSpacing = ImVec2(6.00f, 6.00f);
 	style.TouchExtraPadding = ImVec2(0.00f, 0.00f);
 	style.IndentSpacing = 25;
@@ -364,11 +362,11 @@ void ModuleEditor::SetImGuiTheme() {
 	style.WindowBorderSize = 1;
 	style.ChildBorderSize = 1;
 	style.PopupBorderSize = 1;
-	style.FrameBorderSize = 1;
-	style.TabBorderSize = 1;
+	style.FrameBorderSize = 0;
+	style.TabBorderSize = 0;
 	style.WindowRounding = 7;
 	style.ChildRounding = 4;
-	style.FrameRounding = 3;
+	style.FrameRounding = 12;
 	style.PopupRounding = 4;
 	style.ScrollbarRounding = 9;
 	style.GrabRounding = 3;
